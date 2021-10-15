13d9d340407dfc73cf406b5d95b6c750b972ab62
Baptiste Firket <baptiste.firket@stepstone.com>
2021-10-15 17:15:49 +0200

    404 page

--- src/page/NotFound.jsx
+++ src/page/NotFound.jsx
@@ -1,1 +1,35 @@
-export const NotFound = () => <h1>NotFound</h1>
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import { Main } from '../styles'
+
+const PageContainer = styled(Main)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  div {
+    font-size: 18px;
+  }
+  h1 {
+    font-size: 80px;
+  }
+  h2 {
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+  a {
+    text-decoration: underline;
+  }
+`
+
+export const NotFound = () => (
+  <PageContainer>
+    <div>
+      <h1>Oops.</h1>
+      <h2>The page you&#39;re trying to reach doesn&#39;t exist.</h2>
+      <div>
+        Go back to <Link to="/">Homepage</Link>
+      </div>
+    </div>
+  </PageContainer>
+)
