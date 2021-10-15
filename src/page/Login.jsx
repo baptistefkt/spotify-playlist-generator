401ac4006a096621f9d9fc1afafbbe3739e03ba1
Baptiste Firket <baptiste.firket@stepstone.com>
2021-10-15 15:06:00 +0200

    Login page V1

--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,15 +1,23 @@
+import styled from 'styled-components'
 import { authEndpoint, clientId, redirectUri, scopes } from '../constants'
 import { generateRandomString } from '../helpers'
+import { Main, Button } from '../styles'
+
+const PageContainer = styled(Main)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 const state = generateRandomString(16)
 const href = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes}&response_type=token&show_dialog=true&state=${state}`
 
 export const Login = () => {
   return (
-    <div>
-      <button>
+    <PageContainer>
+      <Button>
         <a href={href}>LOG IN TO SPOTIFY</a>
-      </button>
-    </div>
+      </Button>
+    </PageContainer>
   )
 }
